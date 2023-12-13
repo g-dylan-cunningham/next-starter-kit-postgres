c5f845fa0182494da17732b9ac4e2b7255a0f3f6
G Dylan Cunningham <gdylanc@Gs-MacBook-Air.local>
2023-12-13 16:55:22 -0700

    works on vercel deploy issues

--- knexfile.ts
+++ knexfile.ts
@@ -1,25 +1,15 @@
-// require('dotenv').config({ path: '.env.development'});
+require('dotenv').config({ path: '.env.development' });
 
 import { Knex } from 'knex';
 
 const config: Knex.Config = {
   client: 'pg',
-  connection: "postgresql://postgres@localhost:5432/webapp_dev?sslmode=require&ssl=true&debug=true",
-  //  {
-  //   connectionString: "postgresql://postgres@localhost:5432/webapp_dev?sslmode=require&ssl=true&debug=true",
-  //   //  process.env.POSTGRES_URL,
-  //   ssl: {
-  //     sslmode: "require", 
-  //     rejectUnauthorized: false,
-  //   }
-  // },
-  // connection: process.env.POSTGRES_URL, // + (process.env.NODE_ENV === 'development' ? '' : '?sslmode=require'),
+  connection:
+    process.env.POSTGRES_URL +
+    (process.env.NODE_ENV === 'development' ? '' : '?sslmode=require'),
   migrations: {
     extension: 'ts',
   },
-  pool: {
-    min: 0
-  }
 };
 
 export default config;
