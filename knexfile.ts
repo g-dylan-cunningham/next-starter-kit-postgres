7b1cab73600098f2373f395e04899006b4dae46a
G Dylan Cunningham <gdylanc@Gs-MacBook-Air.local>
2023-12-13 16:40:21 -0700

    refactors knex file

--- knexfile.ts
+++ knexfile.ts
@@ -4,11 +4,15 @@ import { Knex } from 'knex';
 
 const config: Knex.Config = {
   client: 'pg',
-  connection: {
-    connectionString: "postgresql://postgres@localhost:5432/webapp_dev?sslmode=require&ssl=true",
-    //  process.env.POSTGRES_URL,
-    ssl: { rejectUnauthorized: false }
-  },
+  connection: "postgresql://postgres@localhost:5432/webapp_dev?sslmode=require&ssl=true&debug=true",
+  //  {
+  //   connectionString: "postgresql://postgres@localhost:5432/webapp_dev?sslmode=require&ssl=true&debug=true",
+  //   //  process.env.POSTGRES_URL,
+  //   ssl: {
+  //     sslmode: "require", 
+  //     rejectUnauthorized: false,
+  //   }
+  // },
   // connection: process.env.POSTGRES_URL, // + (process.env.NODE_ENV === 'development' ? '' : '?sslmode=require'),
   migrations: {
     extension: 'ts',
