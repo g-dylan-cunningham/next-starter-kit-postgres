02bdfcdd8c9c1b824fb93e601d05739131379da6
G Dylan Cunningham <gdylanc@Gs-MacBook-Air.local>
2023-12-13 16:12:16 -0700

    refactors env vars

--- knexfile.ts
+++ knexfile.ts
@@ -1,10 +1,10 @@
-require('dotenv').config({ path: '.env.development'});
+// require('dotenv').config({ path: '.env.development'});
 
 import { Knex } from 'knex';
 
 const config: Knex.Config = {
   client: 'pg',
-  connection: process.env.POSTGRES_URL + (process.env.NODE_ENV === 'development' ? '' : '?sslmode=require'),
+  connection: process.env.POSTGRES_URL, // + (process.env.NODE_ENV === 'development' ? '' : '?sslmode=require'),
   migrations: {
     extension: 'ts',
   },
